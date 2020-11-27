01b1294fd2b3fafd055d5d72c7130f18c6674057
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2020-11-26 18:08:52 -0700

    Finished

---
+++ src/Question.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = ({id,title, info}) => {
+    const [readMore, setReadMore] = useState(false)
+
+  return <article className='question'> 
+            <header>
+                <h4>{title}</h4>
+                <button className='btn'  onClick={() => setReadMore(!readMore)}>{readMore? <AiOutlineMinus /> : <AiOutlinePlus />}</button>
+            </header>
+            <p>{readMore? info: `${info.substring(0,0)}`}</p>
+  </article>
+};
+
+export default Question;
